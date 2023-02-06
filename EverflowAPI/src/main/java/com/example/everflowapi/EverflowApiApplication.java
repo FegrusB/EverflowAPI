de416c50449d4bb165d413aeb65e700ed749e4d2
FegrusB <fergus@beckerleg.co.uk>
2023-02-06 20:19:54 +0000

    Git Initial setup

---
+++ EverflowAPI/src/main/java/com/example/everflowapi/EverflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.everflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverflowApiApplication.class, args);
+    }
+
+}
---
+++ EverflowAPI/src/test/java/com/example/everflowapi/EverflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
