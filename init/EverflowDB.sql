4689d7166faac0d6e0eff18e4cc92ac433926e60
FegrusB <fergus@beckerleg.co.uk>
2023-02-07 11:49:28 +0000

    07/02/2023 added DB Init file, and data files

--- init/EverflowDB.sql
+++ init/EverflowDB.sql
@@ -2,3 +2,32 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE 'spids' (
+    '_id' int(11) NOT NULL AUTO_INCREMENT,
+    'spid' TINYTEXT,
+    'MeterSerial' TINYTEXT,
+    'MeterManufacturer' TINYTEXT,
+    'MeterSewerageSize' SMALLINT(),
+    'MeterWaterSize' SMALLINT(),
+    'YearlyVolumeEstimate' FLOAT(),
+    'MeterType' BOOlEAN,
+    'ReturnToSewer' SMALLINT(),
+    'GeneralSPID' TINYTEXT,
+    'NumberOfReadDigits' TINYINT,
+    'MeterLocationDescription' TEXT,
+    'MeterReadFrequency' TINYINT,
+    PRIMARY KEY ('_id')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE 'meterReadings' (
+    '_id' int() NOT NULL AUTO_INCREMENT,
+    'MeterSerial' TINYTEXT,
+    'ReadingDate' SMALLDATETIME,
+    'Reading' int,
+    'UsedForEstimate'BOOlEAN,
+    'ManualReading' BOOlEAN,
+    'Rollover' BOOlEAN,
+    'ReadType' CHAR,
+    'GeneralSPID' TINYTEXT,
+    PRIMARY KEY ('_id')
+)
