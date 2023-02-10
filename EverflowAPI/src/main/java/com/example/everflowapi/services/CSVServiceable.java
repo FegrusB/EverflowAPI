05b2872afe7fd44b9224f951e3a0171728b1db7f
FegrusB <fergus@beckerleg.co.uk>
2023-02-10 14:32:35 +0000

    final issue solving

--- EverflowAPI/src/main/java/com/example/everflowapi/services/CSVServiceable.java
+++ EverflowAPI/src/main/java/com/example/everflowapi/services/CSVServiceable.java
@@ -1,6 +1,5 @@
 package com.example.everflowapi.services;
 
-import com.example.everflowapi.helpers.CSVHelper;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface CSVServiceable {
--- EverflowAPI/src/test/java/com/example/everflowapi/MeterReadingsTests.java
+++ EverflowAPI/src/test/java/com/example/everflowapi/MeterReadingsTests.java
@@ -7,10 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.io.BufferedReader;
-import java.io.IOException;
+
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Timestamp;
 
 @SpringBootTest
@@ -19,32 +17,30 @@ public class MeterReadingsTests {
     @Autowired
     SpidRepository repository;
     @Test
-    void csvToMeterReadingTestPerfectSet() throws IOException {
+    void csvToMeterReadingTestPerfectSet(){
 
         MeterReading sampleReading = new MeterReading(repository.findBySpid("3024422755W10"),"08M161403", Timestamp.valueOf("2017-12-17 00:00:00"),3532,false,false,false,"C","3024422755");
 
         InputStream in = getClass().getResourceAsStream("/Data/test1.csv");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         CSVHelper.CSVResult<MeterReading> result = CSVHelper.csvToMeterReading(in,repository);
 
-        Assertions.assertTrue(sampleReading.equals(result.getData().get(0)));
+        Assertions.assertEquals(sampleReading, result.getData().get(0));
         Assertions.assertEquals(10,result.getNumSuccess());
         Assertions.assertEquals(0,result.getNumMissed());
 
     }
 
     @Test
-    void csvToMeterReadingTestImperfectSet() throws IOException {
+    void csvToMeterReadingTestImperfectSet(){
 
         MeterReading sampleReading = new MeterReading(repository.findBySpid("3022317867W11"),"88129662",Timestamp.valueOf("2022-08-15 00:00:00"),22000,false,true,false,"0","3022317867");
 
         InputStream in = getClass().getResourceAsStream("/Data/meter_readings - meter_readings.csv");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         CSVHelper.CSVResult<MeterReading> result = CSVHelper.csvToMeterReading(in,repository);
 
-        Assertions.assertTrue(sampleReading.equals(result.getData().get(157)));
+        Assertions.assertEquals(sampleReading, result.getData().get(157));
         Assertions.assertEquals(158,result.getNumSuccess());
         Assertions.assertEquals(1,result.getNumMissed());
 
